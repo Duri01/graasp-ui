4a1231e06f83065b272028d81000747c4be33451
juancarlosfarah <juancarlos.farah@pm.me>
2020-08-05 18:45:51 +0200

    Updates

---
+++ precache-manifest.b35df6739058c9910f9ff4295dd6805c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52b31cae3bbd04b32355c8db07b190a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "045a3f1ffe44d3ae531e",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "b58db93e358e5b264eb5",
+    "url": "./static/js/2.b04cf699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b04cf699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045a3f1ffe44d3ae531e",
+    "url": "./static/js/main.f800b6cc.chunk.js"
+  },
+  {
+    "revision": "a994ba039f8bcbb02216",
+    "url": "./static/js/runtime-main.c7af9e21.js"
+  }
+]);
